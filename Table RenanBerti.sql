1ccbf8bdfd750430dd538f30afa36e944ddc8088
Renan Berti PC <renanrbsc@gmail.com>
2020-01-09 13:18:35 -0300

    codes CRUD test MySql

--- Table RenanBerti.sql
+++ Table RenanBerti.sql
@@ -3,31 +3,31 @@
 
 -- Command Create Table
 CREATE TABLE RenanBerti (
-  ID int(11) NOT NULL /*AUTO_INCREMENT*/,
+  ID int(11) NOT NULL AUTO_INCREMENT,
   Nome varchar(100) NOT NULL,
   Sobrenome varchar(100) NOT NULL,
   Idade int(11) NOT NULL,
   PRIMARY KEY (ID)
-) /*ENGINE=InnoDB DEFAULT CHARSET=latin1*/
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
 -- Commands Inserts
-INSERT INTO topskills01.RenanBerti (Nome,Sobrenome,Idade)
+INSERT INTO RenanBerti (Nome,Sobrenome,Idade)
 	VALUES ('Renan','Berti',21);
-INSERT INTO topskills01.RenanBerti (Nome,Sobrenome,Idade)
+INSERT INTO RenanBerti (Nome,Sobrenome,Idade)
 	VALUES ('Bruno','Berti',16);
-INSERT INTO topskills01.RenanBerti (Nome,Sobrenome,Idade)
+INSERT INTO RenanBerti (Nome,Sobrenome,Idade)
 	VALUES ('Gabriel','Parasky',8);
 
 -- Command Select row
 SELECT ID, Nome, Sobrenome, Idade
-FROM topskills01.RenanBerti;
+FROM RenanBerti;
 
 --Command delete row
-DELETE FROM topskills01.RenanBerti
+DELETE FROM RenanBerti
 WHERE ID=1;
 
 --Command Update row
-UPDATE topskills01.RenanBerti
+UPDATE RenanBerti
 SET Nome='Bruno', Sobrenome='Berti', Idade=16
 WHERE ID=1;
 
