951a4987b313989e84b58f6a643a70b5dca5c5ea
Renan Berti PC <renanrbsc@gmail.com>
2020-01-09 13:19:51 -0300

    .

--- ProcedureTeste.sql
+++ ProcedureTeste.sql
@@ -1,24 +1,4 @@
 --procedure row span
-CREATE PROCEDURE IRINEU()
-BEGIN
-    DECLARE i int DEFAULT 1;
-    WHILE i <= 50 DO
-   		insert into Keunan (nome,sobrenome,idade) values ('IRINEU','Nao fui eu',2 ); 
-        SET i = i + 1;
-    END WHILE;
-END
-
-call IRINEU();
-	
---Procedure delete span   
-CREATE PROCEDURE IRINEUDELET()
-BEGIN
-   		Delete from keunan where nome = 'IRINEU'; 
-END
-
-CALL IRINEUDELET();
-
---procedure row span
 CREATE PROCEDURE Add_loop_in_RenanBerti()
 BEGIN
     DECLARE i int DEFAULT 1;
@@ -29,3 +9,26 @@ BEGIN
 END
 
 call Add_loop_in_RenanBerti()
+--
+
+--procedure row span
+CREATE PROCEDURE IRINEU()
+BEGIN
+    DECLARE i int DEFAULT 1;
+    WHILE i <= 50 DO
+   		insert into Keunan (nome,sobrenome,idade) values ('IRINEU','Nao fui eu',2 ); 
+        SET i = i + 1;
+    END WHILE;
+END
+
+call IRINEU();
+--
+
+--Procedure delete span   
+CREATE PROCEDURE IRINEUDELET()
+BEGIN
+   		Delete from keunan where nome = 'IRINEU'; 
+END
+
+CALL IRINEUDELET();
+--
