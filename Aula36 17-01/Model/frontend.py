f8f6f48a2af2f89c69007f745263eb83986950b7
Lucas Henrique <luc.ucas388@gmail.com>
2020-01-22 10:07:11 -0300

    ...

--- Aula36 17-01/Model/frontend.py
+++ Aula36 17-01/Model/frontend.py
@@ -1,12 +1,8 @@
-class Squads:
+class FrontEnd:
     
     def __init__(self):   
-        self.codigo: 0
-        self.name_squad = ''
-        self.descricao = ''
-        self.numero_pessoas = 0
-        self.linguagembackend = ''
-        self.linguagemfrontend = ''
-    
+        self.ID= 0
+        self.LINGUAGEMBACKEND = ''
+
     def __str__(self):
-        return f'{self.codigo};{self.name_squad};{self.descricao};{self.numero_pessoas};{self.linguagembackend};{self.linguagemfrontend}'
+        return f'{self.codigo};{self.LINGUAGEMBACKEND}'
