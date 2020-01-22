cf98d8ef83850a65bd22eb844260cd4fe0e6d91b
Renan <renanrbsc@gmail.com>
2020-01-22 11:35:43 -0300

    ...

--- Aula36 17-01/Controller/squads_controller.py
+++ Aula36 17-01/Controller/squads_controller.py
@@ -1,8 +1,13 @@
 from Dao.squads_dao import SquadsDao
 from Model.squads import Squads
+from Controller.frontend_controller import FrontController
+from Controller.backend_controller import BackController
 
 class SquadsController:
     dao = SquadsDao()
+    backend_controller = BackController()
+    frontend_controller = FrontController()
+    
 
     def listar_todos(self):
         return self.dao.listar_todos()
