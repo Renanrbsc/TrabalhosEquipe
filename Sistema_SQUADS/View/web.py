97be11da05de6781db349127b38408990d1d2a1f
Renan Berti PC <renanrbsc@gmail.com>
2020-01-26 05:07:20 -0300

    corrigindo diretorio e diminuindo arquivo duplicado

--- Sistema_SQUADS/View/web.py
+++ Sistema_SQUADS/View/web.py
@@ -177,7 +177,7 @@ def recebe_alterar_dados():
 
     sqcontroller.alterar(squad,id)
     tupla = sqcontroller.listar_por_id(id)
-    return render_template('squad_atualizado.html',titulo_app = name, dados = tupla)
+    return render_template('listar_codigo.html', titulo_app = name, dados = tupla)
 
 @app.route('/alterar/linguagem')
 def alterar_front():
